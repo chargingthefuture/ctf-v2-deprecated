a8644d66798e50767140b2b787fc8c0f92c7d7c5
Farah Brunache <16567092+farahbrunache@users.noreply.github.com>
2026-04-02 18:36:58 +0000

    updated next config

--- ctf/packages/web/next.config.js
+++ ctf/packages/web/next.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
 const { withSentryConfig } = require('@sentry/nextjs');
 const shouldSkipSentryBuildPlugin = process.env.CTF_SKIP_SENTRY_NEXTJS === '1';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.join(__dirname, '..', '..', '..'),
   reactStrictMode: true,
   typescript: {
     tsconfigPath: './tsconfig.json',
